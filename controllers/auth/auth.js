15b9bbdc85173acdc186f78cde4ac7e1576e3b76
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-07-05 22:40:19 +0300

    Update auth.js

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -2,7 +2,8 @@ const userModel = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET_KEY = process.env.TOKEN_KEY;
-
+const crypto = require("crypto");
+const sendEmail = require("../../sendEmail");
 function generateAuthToken(data) {
   const token = jwt.sign(data, JWT_SECRET_KEY, { expiresIn: "10h" });
   return token;
@@ -37,7 +38,7 @@ module.exports.login = async (req, res) => {
     return res.json({
       success: false,
       status: 400,
-      message: "user credentials are not correct",
+      message: "email or password is incorrect",
     });
   } catch (error) {
     return res.send(error.message);
@@ -129,32 +130,117 @@ module.exports.userById = async (req, res) => {
   }
 };
 
-module.exports.resetPassword = async (req, res) => {
+
+
+module.exports.forgetPassword = async (req, res, next) => {
   try {
-    const { password, newPassword } = req.body;
-    const { id } = req.query;
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return next(
+        res
+          .status(404)
+          .send(`there is no user with this email ${req.body.email}`)
+      );
+    }
+    //2-if user exist , generate random 6 digits and save it in db and encrypt it to protect from hacking
+    const ResetCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const hashedRestCode = crypto
+      .createHash("sha256")
+      .update(ResetCode)
+      .digest("hex");
 
-    if (!password || !newPassword || !id) return res.send("Fields are empty");
+    //save hashed reset code into db
+    user.passwordRestCode = hashedRestCode;
+    //add expiration time to rest code (10 min)
+    user.passwordRestExpires = Date.now() + 10 * 60 * 1000; //mille second to second
+    user.passwordRestVerified = false;
 
-    let user = await userModel.findOne({ _id: id });
+    await user.save();
+    //3-send the reset code via email
+    const message = `Hi ${user.name} ,\n We received a request to reset the password on your EasyBuy. \n${ResetCode} \n Enter this code to complete the reset \n thanks for helping us keep your account secure \n the EasyBuy team`;
 
-    if (!user) return res.send("user does not exist");
-
-    // comparing the password from the password in DB to allow changes
-    if (bcrypt.compare(password, user?.password)) {
-      // encrypting new password
-      user.password = await bcrypt.hash(newPassword, 10);
-      user.save();
-      return res.json({
-        success: true,
-        message: "password updated successfully",
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: "Your Password Rest Code (Valid For 10 Min) ",
+        message,
       });
+    } catch (error) {
+      user.passwordRestCode = undefined;
+      user.passwordRestExpires = undefined;
+      user.passwordRestVerified = undefined;
+      await user.save();
+      return next(
+        res.status(500).send("there is an error in sending to email")
+      );
     }
 
-    return res.json({
-      success: false,
-      message: "wrong password",
-    });
+    res
+      .status(200)
+      .json({ status: "success", message: "reset code sent to email" });
+  } catch (error) {
+    return res.send(error.message);
+  }
+};
+
+module.exports.verifyResetCode = async (req, res, next) => {
+  try {
+    //1) check reset code you enter is = reset code you send
+    const hashedRestCode = crypto
+      .createHash("sha256")
+      .update(req.body.resetCode)
+      .digest("hex");
+    //2)  get user by reset number
+    const user = await userModel.findOne({ passwordRestCode: hashedRestCode });
+    if (!user) {
+      return next(res.status(404).send("Reset code invalid"));
+    }
+    //3) check reset code is  expired
+    const checkExpired = await userModel.findOne({
+      passwordRestExpires: { $gt: Date.now() },
+    }); //لازم يكون وقت الانتهاء اكبر من الوقت اللي انا بدخله فيه
+    if (!checkExpired) {
+      return next(res.status(401).send("Reset code expired"));
+    }
+    //4)valid rest code
+    user.passwordRestVerified = true;
+    await user.save();
+
+    res.status(200).json({ message: "verified successfully" });
+  } catch (error) {
+    return res.send(error.message);
+  }
+};
+
+module.exports.resetPassword = async (req, res, next) => {
+  try {
+    //1) check user verified rest code
+    //get user by email
+    const { email } = req.body;
+    let user = await userModel.findOne({ email });
+    if (!user) {
+      return next(res.status(404).send("No user with this Email "));
+    }
+    //2)if verify is true
+    if (!user.passwordRestVerified) {
+      return next(
+        res.status(400).send("We send a Reset Code ,Please verify your email")
+      );
+    }
+
+    //3) set new password
+    user.password = await bcrypt.hash(req.body.newPassword, 10);
+    user.passwordRestCode = undefined;
+    user.passwordRestExpires = undefined;
+    user.passwordRestVerified = undefined;
+    await user.save();
+    //if every thing is ok generate new token
+
+    const token = generateAuthToken({ _id: user?._id, email: email });
+    user.token = token;
+    await user.save();
+
+    res.status(200).json({ message: "new password set successfully", token });
   } catch (error) {
     return res.send(error.message);
   }
