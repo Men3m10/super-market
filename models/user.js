ce2f8a69de672d11af61dc9d15f66585b6d3e666
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-07-05 22:39:34 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,21 +1,39 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 // const { TOKEN_KEY } = process.env
 
-const userSchema = mongoose.Schema({
-    name : String,
-    email : {type : String, required : true, unique : true},
-    userType : String,
-    password : String,
-    token : String,
-    wishlist : [{productId : {type: mongoose.Schema.Types.ObjectId, ref : "product"}, quantity : Number}]
-
-},{timestamps: true})
-
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: [true, "email required"],
+    },
+    userType: String,
+    password: String,
+    token: String,
+    wishlist: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "product" },
+        quantity: Number,
+      },
+    ],
+    passwordRestCode: String,
+    passwordRestExpires: Date,
+    passwordRestVerified: Boolean,
+  },
+  { timestamps: true }
+);
 
 // userSchema.methods.generateAuthToken = function () {
 //     this.token = jwt.sign({ userID: this._id, email: this.email }, TOKEN_KEY, { expiresIn: '10h' })
 // }
 
-module.exports = mongoose.model('user',userSchema)
+module.exports = mongoose.model("user", userSchema);
