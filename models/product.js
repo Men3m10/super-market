7c0b1b40e95505448471b3e25bc8a4c8c78c8b59
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-07-06 21:34:04 +0300

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,15 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+const productSchema = mongoose.Schema(
+  {
+    title: String,
+    sku: { type: String },
+    price: Number,
+    image: String,
+    imageId: String,
+    description: String,
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
+    quantity: Number,
+  },
+  { timestamps: true }
+);
 
-    title : String,
-    sku : {type : String},
-    price : Number,
-    image : String,
-    description : String,
-    category : {type:mongoose.Schema.Types.ObjectId, ref:'categories'},
-    quantity : Number
-
-}, { timestamps : true })
-
-module.exports = mongoose.model("product", productSchema)
+module.exports = mongoose.model("product", productSchema);
