53d938e1b3aac8ef5a40e3e39c3e33e4911d6bb4
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-07-11 19:30:33 +0300

    Update auth.js

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -90,7 +90,7 @@ module.exports.changePassword = async (req, res, next) => {
     if (!findUser || !(await bcrypt.compare(password, findUser.password))) {
       return res.status(401).json({
         success: false,
-        message: "password is incorrect",
+        message: " current password is incorrect",
       });
     }
     if (!password || !newPassword) {
